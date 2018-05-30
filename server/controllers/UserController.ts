a1625c2a34908faf1769f0cdc3ebc237851fa562
AntoinePoree <poree47@gmail.com>
2018-05-30 19:09:16 +0200

    ??

--- server/controllers/UserController.ts
+++ server/controllers/UserController.ts
@@ -1,28 +1,28 @@
-import * as dotenv from "dotenv";
-import * as jwt from "jsonwebtoken";
+import * as dotenv from 'dotenv';
+import * as jwt from 'jsonwebtoken';
 
-import userModel from "../models/userModel";
-import BaseController from "./BaseController";
-const simpleOauthModule = require("simple-oauth2");
-const https = require("https");
+import userModel from '../models/userModel';
+import BaseController from './BaseController';
+const simpleOauthModule = require('simple-oauth2');
+const https = require('https');
 
 const oauth2 = simpleOauthModule.create({
   client: {
-    id: "5b473f25ae3b81262abce0595e896c5876bb56fd8604863b724ae792aece495e",
-    secret: "851f080ed080e7aac1a869b47bf5b1a33cfdfbcfd6d1c19ecd886fc41683c85e"
+    id: '5b473f25ae3b81262abce0595e896c5876bb56fd8604863b724ae792aece495e',
+    secret: '851f080ed080e7aac1a869b47bf5b1a33cfdfbcfd6d1c19ecd886fc41683c85e',
   },
   auth: {
-    tokenHost: "https://ppody.innoveduc.fr",
-    tokenPath: "/oauth/token",
-    authorizePath: "/oauth/authorize"
-  }
+    tokenHost: 'https://ppody.innoveduc.fr',
+    tokenPath: '/oauth/token',
+    authorizePath: '/oauth/authorize',
+  },
 });
 
 // Authorization uri definition
 const authorizationUri = oauth2.authorizationCode.authorizeURL({
-  redirect_uri: "http://localhost:4200/api/oauth/callback",
-  scope: "",
-  state: ""
+  redirect_uri: 'http://localhost:4200/api/oauth/callback',
+  scope: '',
+  state: '',
 });
 
 export default class UserController extends BaseController {
@@ -41,21 +41,21 @@ export default class UserController extends BaseController {
         res.status(200).json({ token });
       });
     });
-  };
+  }
 
   oauth = (req, res) => {
     console.log(authorizationUri);
     res.redirect(authorizationUri);
-  };
+  }
 
   // Callback service parsing the authorization token and asking for the access token
   callback = async (req, res) => {
     const code = req.query.code;
     const options = {
       code,
-      redirect_uri: "http://localhost:4200/api/oauth/callback",
-      scope: "",
-      state: ""
+      redirect_uri: 'http://localhost:4200/api/oauth/callback',
+      scope: '',
+      state: '',
     };
 
     console.log(code);
@@ -67,12 +67,12 @@ export default class UserController extends BaseController {
 
       return res.status(200).json(token);
     } catch (error) {
-      console.error("Access Token Error", error);
-      return res.status(500).json("Authentication failed");
+      console.error('Access Token Error', error);
+      return res.status(500).json('Authentication failed');
     }
-  };
+  }
 
   success = (req, res) => {
-    res.send("");
-  };
+    res.send('');
+  }
 }
