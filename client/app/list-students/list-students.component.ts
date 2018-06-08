7bdba6bce79c2a9f740e35a4af1a1bb56211e042
wilder <champion.bas@gmail.com>
2018-06-08 09:50:00 +0200

    dynamic list-students

--- client/app/list-students/list-students.component.ts
+++ client/app/list-students/list-students.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StudentService } from '../services/student.service';
+import { Student } from '../shared/models/student.model';
 
 
 @Component({
@@ -9,10 +10,26 @@ import { StudentService } from '../services/student.service';
   styleUrls: ['./list-students.component.css']
 })
 export class ListStudentsComponent implements OnInit {
+  students: Student[];
+  isLoading = true;
+  isEditing = false;
+  
+  constructor(private studentService: StudentService) { }
 
-  constructor() { }
+
 
   ngOnInit() {
+    this.getStudent();
   }
 
+  getStudent() {
+    this.studentService.getStudents().subscribe(
+      (data) => {
+        console.log(data);
+        this.students = data;
+      },
+      error => console.log(error),
+      () => this.isLoading = false,
+    );
+  }
 }
