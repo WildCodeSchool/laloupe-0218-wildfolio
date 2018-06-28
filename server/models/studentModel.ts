ba1ea21020bd53aa0db470f0d9265cfed742c3d6
AntoinePoree <poree47@gmail.com>
2018-06-28 14:38:52 +0200

    Add back image and linkedin

--- server/models/studentModel.ts
+++ server/models/studentModel.ts
@@ -6,6 +6,8 @@ const studentSchema = new mongoose.Schema({
   email: String,
   WCS_ID: Number,
   github: String,
+  linkedin: String,
+  image: String,
   roles: String,
   admin: Boolean,
   banished: Boolean,
