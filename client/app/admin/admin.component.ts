d84e160f290fdd1ac7a82b6940d54ec99a89c39b
AntoinePoree <poree47@gmail.com>
2018-07-02 16:46:51 +0200

    Add comiit

--- client/app/admin/admin.component.ts
+++ client/app/admin/admin.component.ts
@@ -1,7 +1,9 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { ToastComponent } from '../shared/toast/toast.component';
 import { User } from '../shared/models/user.model';
+import { StudentService } from '../services/student.service';
 
 @Component({
   selector: 'app-admin',
@@ -12,13 +14,33 @@ export class AdminComponent implements OnInit {
 
   users: User[] = [];
   isLoading = true;
-
+  id: string;
+  me;
   constructor(
     public toast: ToastComponent,
+    private studentService: StudentService,
+    private route: ActivatedRoute,
+
   ) { }
 
   ngOnInit() {
-    
-  }
+    this.getMe();
+    this.id = this.route.snapshot.paramMap.get('id');
 
+  }
+  getMe() {
+    this.studentService.getMe().subscribe(
+      (data) => {
+        this.me = data,
+        console.log(this.me);
+      },
+      error => console.log(error),
+      () => this.isLoading = false,
+  );
+  }
+  verifyRoles() {
+    if (this.me = false) {
+      console.log('Admin');
+    }
+  }
 }
