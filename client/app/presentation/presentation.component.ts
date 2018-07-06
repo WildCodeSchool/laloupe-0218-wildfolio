8bf216edebde3bca8884feae2ef5f56e8f7ac3c0
AntoinePoree <poree47@gmail.com>
2018-07-06 11:18:15 +0200

    Add search language in homecomponent

--- client/app/presentation/presentation.component.ts
+++ client/app/presentation/presentation.component.ts
@@ -1,5 +1,6 @@
+import { LangageService } from './../services/langage.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Recrut } from '../shared/models/recrut.model';
 import { RecrutService } from '../services/recrut.service';
 import * as AOS from 'aos';
@@ -9,6 +10,7 @@ import { WcsService } from '../wcs.service';
 import { BlogProjetService } from '../services/blogProjet.service';
 import { BlogProjet } from '../shared/models/blogProjet.model';
 import { ELEMENT_PROBE_PROVIDERS } from '@angular/platform-browser/src/dom/debug/ng_probe';
+import { Langage } from '../shared/models/langage.model';
 
 @Component({
   selector: 'app-presentation',
@@ -25,17 +27,23 @@ export class PresentationComponent implements OnInit {
   recruts: Recrut[] = [];
   students: Student[];
   student = new Student();
+  selectedLangage: any;
+  langage = new Langage();
+  langages: Langage[] = [];
 
   constructor(
     route: ActivatedRoute,
     private recrutService: RecrutService,
     private studentService: StudentService,
     private blogProjetService: BlogProjetService,
+    private langageService: LangageService,
+    private router: Router,
   ) { }
 
   ngOnInit() {
     this.getBlogProjet();
     this.getStudent();
+    this.getLangage();
     this.getRecrut();
     AOS.init({
       duration: 800,
@@ -43,7 +51,16 @@ export class PresentationComponent implements OnInit {
     });
 
   }
-
+  getLangage() {
+    this.langageService.getLangages().subscribe(
+      (data) => {
+        console.log(data);
+        this.langages = data;
+      },
+      error => console.log(error),
+      () => (this.isLoading = false),
+    );
+  }
   // Oeil du recruteur
 
   getRecrut() {
@@ -71,17 +88,32 @@ export class PresentationComponent implements OnInit {
     );
   }
 
-   // Développeurs
+  // Développeurs
 
   getStudent() {
     this.studentService.getStudents().subscribe(
       (data) => {
         console.log(data);
         this.students = data;
+        console.log(Math.floor(Math.random() * this.student.WCS_ID));
         return Math.floor(Math.random() * this.student.WCS_ID);
       },
       error => console.log(error),
       () => this.isLoading = false,
     );
+  }
+  getLangageById() {
+    this.studentService.getLangageById(this.selectedLangage).subscribe(
+      (data) => {
+        this.students = data;
+        console.log(data);
+      },
+      error => console.log(error),
+      () => this.isLoading = false,
+    );
+  }
+  showDev() {
+    this.router.navigate(['/list-students']);
+    this.getLangageById();
   }
 }
