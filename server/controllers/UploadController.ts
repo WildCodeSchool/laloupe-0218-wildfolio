89c53197397c166395706337919e7d6c66662e13
AntoinePoree <poree47@gmail.com>
2018-07-13 13:36:04 +0200

    Fixed upload

--- server/controllers/UploadController.ts
+++ server/controllers/UploadController.ts
@@ -125,7 +125,7 @@ export default class UploadController extends BaseController {
         });
         readstream.on('close', (file) => {
           client.close();
-          console.log('close :', file.filename);
+          console.log('close :', __filename);
         });
       },
     );
